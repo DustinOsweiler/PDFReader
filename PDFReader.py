fda270fac1a411cdc70f4089f78712f4d1177a12
Dustin Osweiler <dosweiler@carthage.edu>
2021-03-17 21:38:56 -0500

    Update PDFReader.py

--- PDFReader.py
+++ PDFReader.py
@@ -1,20 +1,17 @@
 import PyPDF2
-import subprocess
-import os
-import fitz
 import re
+import os
+import subprocess
+import sys
 import tkinter as tk
 import PDFGui
 from PDFGui import Application
 
-#root = tk.Tk()
-#app = Application(master=root)
-#app.mainloop()
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
 
-#program = PDFReaderGui()
-#program.window.mainloop()
-
-#if program.all_input_selected == True:
+if app.all_input_selected == True:
 
     #Opens and reads the PDF File
     pdffile = open('StormwaterandUtilityLocationIndexBookStreetIndexMobil.pdf', 'rb')
@@ -22,51 +19,102 @@ from PDFGui import Application
 
     #Gets the number of pages in the PDF
     numpages = readpdf.getNumPages()
-    #print(numpages)
 
     #Define keyterms
-    Address = save_address
-    print(Address)
+    Street = app.save_Street.lower()
+    
+    Street = Street.replace("avenue", "ave")
+    Street = Street.replace("street", "st")
+    Street = Street.replace("court", "ct")
+    Street = Street.replace("road", "rd")
+    Street = Street.replace("boulevard", "blvd")
+    Street = Street.replace("lane", "ln")
+    Street = Street.replace("drive", "dr")
+    Street = Street.replace("place", "pl")
+    Street = Street.replace("circle", "cir")
 
-    #Parces the address
-    def convert(lst): 
-        return (lst[0].split())
-    lst = Address
+    HouseNumber = app.save_Number
+    print(HouseNumber)
 
-    Wheaton = [5,6,7,8,9,10,11,12,13,15,16,21,22,23,24,25,50,51,52,53,54,55,56,57,58,59,61]
-    Winfield = [1,2,14]
-    Naperville = [60]
-    Glen_Ellyn = [28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,46,47,62,63,64,65,66,67,68,69,70,71,72,73]
-    Lombard = [43,44,45,48,49,72,74,75,76,77]
-    Carol_Stream = [3,4,17,18,19,20,26,27]
+    def is_valid_data(string):
+        ret_val = True
+        if string.count('.') >= 2: 
+            ret_val = False
+        elif string == ' ' or string == '':
+            ret_val = False
+        
+        return ret_val
 
-    print()
+        
+    # creating a page object  
+    firstPage = readpdf.getPage(0)  
+    secondPage = readpdf.getPage(1)
+    
+    # extracting text from page  
+    text = firstPage.extractText() 
+    text2 = secondPage.extractText()
 
-    for i in range(0, 1): #Searches the index for the street name
-        PageObjStreet = readpdf.getPage(i)
-        StreetText = PageObjStreet.extractText()
-        print(streetText)
-        StreetSearch = re.search(lst[1], StreetText, re.IGNORECASE)
-        if(StreetSearch != None): #If we find the addresss we get the page number and put it in a string
-            print("Street found in Index!")
-            for t in range(3, numpages): #Searches the pages gotten from previous for loop for the address
-                PageObjAddress = readpdf.getPage(t)
-                AddressText = PageObjAddress.extractText()
-                AddressSearch = re.search(Address, AddressText, re.IGNORECASE)
-                if(AddressSearch != None):
-                    pagenum = (t) #Adding one because python counts from 0
-                    print(pagenum)
-                    page = 'page=' + str(pagenum)
+    #take off the "Index" at the front of the string
+    text = text + text2
+    split = text.splitlines()
 
-                    path_to_pdf = os.path.abspath('.\StormwaterandUtilityLocationIndexBookStreetIndexMobil.pdf')
+    #create an empty list to put the addresses in 
+    clean_split = []
 
-                    path_to_acrobat = os.path.abspath('C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe') 
+    for val in split:
+        if is_valid_data(val):
+            clean_split.append(val.lower())
+        
+        
+    combined_string = ' '.join(clean_split)
+    print(combined_string)
+    #find the index in the string where the road is 
+    loc = combined_string.find(Street)
 
-                    process = subprocess.Popen([path_to_acrobat, '/A', page, path_to_pdf], shell=False, stdout=subprocess.PIPE)
-                    process.wait()
-                    quit()
+    #as long as it is in the string of names, we will get all of the numbers right after it
+    if loc != -1:
+        #move past the street name itself
+        #while not combined_string[loc].isnumeric() :
+        #    loc += 1
+        loc += len(Street)
+
+        pagesearchnumbers = ""
+    
+        while not combined_string[loc].isalpha():
+            pagesearchnumbers += combined_string[loc]
+            loc += 1
+    
+            if loc >= len(combined_string):
+                break
+
+        pagesearchnumbers = pagesearchnumbers.strip()
+    
+        split_numbers = pagesearchnumbers.split(', ')
+        for i, val in enumerate(split_numbers):
+            print(val)
+            split_numbers[i] = val.split(' ')[0]
+            print(split_numbers[i])
+
+        #cast all of the page numbers as ints
+        numbers = [int(val) + 3 for val in split_numbers]
+        print(numbers)
+
+        for t in (numbers): #Searches the pages gotten from previous for loop for the address
+            PageObjAddress = readpdf.getPage(t)
+            AddressText = PageObjAddress.extractText()
+            print(AddressText)
+            AddressSearch = re.search(HouseNumber, AddressText, re.IGNORECASE)
+            if(AddressSearch != None):
+                pagenum = (t) #Adding one because python counts from 0
+                page = 'page=' + str(pagenum)
+
+                path_to_pdf = os.path.abspath('.\StormwaterandUtilityLocationIndexBookStreetIndexMobil.pdf')
+
+                path_to_acrobat = os.path.abspath('C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe') 
+
+                process = subprocess.Popen([path_to_acrobat, '/A', page, path_to_pdf], shell=False, stdout=subprocess.PIPE)
+                process.wait()
+                sys.exit(0)
 
   
-    print("Address not found")
-
-#Check for Common Typos, Capitalization, Abbreviations
+print("Address not found")
